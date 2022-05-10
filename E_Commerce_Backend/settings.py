014ae45a5c02e3f485142728e04788bcaa7b7b8a
huy <huync21@gmail.com>
2022-05-10 09:15:49 +0700

    add cors to allow react fe call api

--- E_Commerce_Backend/settings.py
+++ E_Commerce_Backend/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'ratings',
     'categories',
     'banners',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -55,6 +56,17 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+]
+
+CORS_ALLOW_ALL_ORIGINS = True  # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]  # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    'http://localhost:3000',
 ]
 
 ROOT_URLCONF = 'E_Commerce_Backend.urls'
